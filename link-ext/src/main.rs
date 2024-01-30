f85cf2a0ca14fd5c327d83aeb09ec6c9af0ea0f0
Jve386 <jve.formacio@gmail.com>
2024-01-30 11:27:02 +0100

    Commented link-ext util

--- link-ext/src/main.rs
+++ link-ext/src/main.rs
@@ -1,7 +1,9 @@
+// Import necessary libraries
 use error_chain::error_chain;
 use select::document::Document;
 use select::predicate::Name;
 
+// Define custom error types using the error_chain macro
 error_chain! {
     foreign_links {
         Reqerror(reqwest::Error);
@@ -9,17 +11,18 @@ error_chain! {
     }
 }
 
+// Define the main asynchronous function
 #[tokio::main]
-async fn main() -> Result <()> {
-    let res = reqwest::get("https://www.rust-lang.org/en-US/")
-        .await?
-        .text()
-        .await?; 
+async fn main() -> Result<()> {
+    // Make a GET request to the Rust Lang website and retrieve the text content
+    let res = reqwest::get("https://www.rust-lang.org/en-US/").await?.text().await?;
 
-Document::from(res.as_str())
-.find(Name("a"))
-.filter_map(|n| n.attr("href"))
-.for_each (|x| println!("{}", x));
+    // Parse the HTML content into a Document and find all 'a' tags
+    Document::from(res.as_str())
+        .find(Name("a"))
+        .filter_map(|n| n.attr("href"))
+        .for_each(|x| println!("{}", x));
 
-Ok(())
+    // Return a successful result
+    Ok(())
 }
